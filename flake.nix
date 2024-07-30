84194899299b1a155f9433c361b1476c1e53385f
Nikita Podvirnyi <krypt0nn@vk.com>
2024-07-30 18:16:15 +0200

    build: updated nix flake

--- flake.nix
+++ flake.nix
@@ -1,39 +1,53 @@
 {
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    naersk.url = "github:nix-community/naersk";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+        nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+        naersk.url = "github:nix-community/naersk";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
 
-  outputs = { self, flake-utils, ... }@inputs:
-    flake-utils.lib.eachSystem [ "x86_64-linux"] (system:
-      let
-        overlay-unstable = final: prev: {
-          unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
-        };
+    outputs = { self, flake-utils, ... }@inputs:
+        flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
+            let
+                overlay-unstable = final: prev: {
+                    unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
+                };
 
-        pkgs = import inputs.nixpkgs {
-          inherit system;
-          overlays = [ overlay-unstable ];
-        };
+                pkgs = import inputs.nixpkgs {
+                    inherit system;
 
-        naersk' = pkgs.callPackage inputs.naersk {};
+                    overlays = [
+                        overlay-unstable
+                    ];
+                };
 
-        fhs = pkgs.buildFHSUserEnv {
-          name = "wincompatlib-dev";
-          targetPkgs = pkgs: with pkgs; [
-            gcc
-            cmake
-            pkg-config
-          ] ++ [ self.defaultPackage ];
-        };
-      in
-      {
-        defaultPackage = naersk'.buildPackage {
-          src = ./.;
-        };
-        devShells.default = fhs.env;
-      }
-    );
+                naersk' = pkgs.callPackage inputs.naersk {};
+
+                fhs = pkgs.buildFHSUserEnv {
+                    name = "wincompatlib-dev";
+
+                    targetPkgs = pkgs: with pkgs; [
+                        pkgs.unstable.rustup
+                        pkgs.unstable.rustfmt
+                        pkgs.unstable.clippy
+
+                        gcc
+                        cmake
+                        pkg-config
+
+                        # Needed for fonts installation
+                        cabextract
+
+                        self.defaultPackage
+                    ];
+                };
+
+            in {
+                defaultPackage = naersk'.buildPackage {
+                    src = ./.;
+                };
+
+                devShells.default = fhs.env;
+            }
+        );
 }
