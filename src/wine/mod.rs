7da96c78111017b52eb7c0c7944355133cc3bc1a
Nikita Podvirnyi <krypt0nn@vk.com>
2025-04-08 15:24:05 +0200

    feat: add `wow64` wine arch

--- src/wine/mod.rs
+++ src/wine/mod.rs
@@ -18,8 +18,14 @@ pub mod bundle;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub enum WineArch {
+    /// 32 bit only wine.
     Win32,
-    Win64
+
+    /// 64 bit only wine.
+    Win64,
+
+    /// 64 bit wine which can run 32 bit apps (since 10.0 release).
+    Wow64
 }
 
 impl WineArch {
@@ -29,6 +35,8 @@ impl WineArch {
         match arch {
             "win32" | "x32" | "32" => Some(Self::Win32),
             "win64" | "x64" | "64" => Some(Self::Win64),
+            "wow64" => Some(Self::Wow64),
+
             _ => None
         }
     }
@@ -37,7 +45,8 @@ impl WineArch {
     pub fn to_str(&self) -> &str {
         match self {
             Self::Win32 => "win32",
-            Self::Win64 => "win64"
+            Self::Win64 => "win64",
+            Self::Wow64 => "wow64"
         }
     }
 }
@@ -112,7 +121,7 @@ pub struct Wine {
     pub wine_libs: WineSharedLibs,
 
     /// Describes which `GST_PLUGIN_PATH` value should be used
-    /// 
+    ///
     /// https://gstreamer.freedesktop.org/documentation/gstreamer/gstregistry.html?gi-language=c
     pub gstreamer_libs: GstreamerSharedLibs
 }
@@ -151,10 +160,10 @@ impl Wine {
     }
 
     /// Try to get version of provided wine binary. Runs command: `wine --version`
-    /// 
+    ///
     /// ```
     /// use wincompatlib::prelude::*;
-    /// 
+    ///
     /// match Wine::default().version() {
     ///     Ok(version) => println!("Wine version: {:?}", version),
     ///     Err(err) => eprintln!("Wine is not available: {}", err)
@@ -182,7 +191,7 @@ impl Wine {
             if let Some(parent) = parent.parent() {
                 let windows = match self.arch {
                     WineArch::Win32 => parent.join("lib/wine/i386-windows"),
-                    WineArch::Win64 => parent.join("lib64/wine/x86_64-windows"),
+                    WineArch::Win64 | WineArch::Wow64 => parent.join("lib64/wine/x86_64-windows")
                 };
 
                 // [wine folder]/lib/wine/i386-windows/[binary]
@@ -207,23 +216,23 @@ impl Wine {
     }
 
     /// Try to get path to wineboot binary
-    /// 
+    ///
     /// If wine binary is specified (so not system), then function will try to find wineboot binary inside of this wine's folder
-    /// 
+    ///
     /// ```no_run
     /// use wincompatlib::prelude::*;
-    /// 
+    ///
     /// use std::path::PathBuf;
-    /// 
+    ///
     /// // Build with wineboot script
     /// assert_eq!(Wine::from_binary("wine_folder/bin/wine").wineboot(), Some(WineBoot::Unix(PathBuf::from("wine_folder/bin/wineboot"))));
-    /// 
+    ///
     /// // Build without wineboot script, but with wineboot.exe file
     /// assert_eq!(Wine::from_binary("wine_folder/bin/wine").wineboot(), Some(WineBoot::Windows(PathBuf::from("wine_folder/lib64/wine/x86_64-windows/wineboot.exe"))));
-    /// 
+    ///
     /// // Build without wineboot script and wineboot.exe file, but this file exists in wine prefix
     /// assert_eq!(Wine::from_binary("wine_folder/bin/wine").with_prefix("path/to/prefix").wineboot(), Some(WineBoot::Windows(PathBuf::from("path/to/prefix/drive_c/windows/system32/wineboot.exe"))));
-    /// 
+    ///
     /// // Builds without any wineboot version
     /// assert_eq!(Wine::from_binary("wine_folder/bin/wine").wineboot(), None);
     /// ```
@@ -249,17 +258,17 @@ impl Wine {
 
     #[inline]
     /// Get path to wineserver binary, or "wineserver" if not specified
-    /// 
+    ///
     /// If wine binary is specified (so not system), then function will try to find wineserver binary inside of this wine's folder
-    /// 
+    ///
     /// ```no_run
     /// use wincompatlib::prelude::*;
-    /// 
+    ///
     /// use std::path::PathBuf;
-    /// 
+    ///
     /// // Build with wineserver
     /// assert_eq!(Wine::from_binary("wine_build_with_wineserver/wine").wineserver(), PathBuf::from("wine_build_with_wineserver/wineserver"));
-    /// 
+    ///
     /// // Build without wineserver
     /// assert_eq!(Wine::from_binary("wine_build_without_wineserver/wine").wineserver(), PathBuf::from("wineserver"));
     /// ```
@@ -280,23 +289,23 @@ impl Wine {
     }
 
     /// Get environment variables map from current struct's values
-    /// 
+    ///
     /// Can contain (if specified in current struct):
-    /// 
+    ///
     /// - `WINEPREFIX`
     /// - `WINEARCH`
     /// - `WINESERVER`
     /// - `WINELOADER`
     /// - `LD_LIBRARY_PATH`
     /// - `GST_PLUGIN_PATH`
-    /// 
+    ///
     /// ```
     /// use wincompatlib::prelude::*;
-    /// 
+    ///
     /// use std::process::Command;
-    /// 
+    ///
     /// let wine = Wine::default().with_arch(WineArch::Win64);
-    /// 
+    ///
     /// Command::new(&wine.binary)
     ///     .envs(wine.get_envs())
     ///     .spawn();
@@ -335,10 +344,10 @@ impl Wine {
     #[cfg(feature = "dxvk")]
     #[inline]
     /// Run `Dxvk::install` with parameters from current Wine struct. Will try to use system-wide binaries if some not specified
-    /// 
+    ///
     /// ```no_run
     /// use wincompatlib::prelude::*;
-    /// 
+    ///
     /// Wine::from_binary("/path/to/wine")
     ///     .with_arch(WineArch::Win64)
     ///     .install_dxvk("/path/to/dxvk-2.1", InstallParams::default())
@@ -351,10 +360,10 @@ impl Wine {
     #[cfg(feature = "dxvk")]
     #[inline]
     /// Run `Dxvk::uninstall` with parameters from current Wine struct. Will try to use system-wide binaries if some not specified
-    /// 
+    ///
     /// ```no_run
     /// use wincompatlib::prelude::*;
-    /// 
+    ///
     /// Wine::from_binary("/path/to/wine")
     ///     .with_arch(WineArch::Win64)
     ///     .uninstall_dxvk(InstallParams::default())
